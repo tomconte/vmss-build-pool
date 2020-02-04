aabe1891ea99431768ae23fda4ec14670cc6b279
Thomas Conte <thomas@conte.com>
2020-02-04 16:14:15 +0100

    Install az, bazel

--- install-build-agent.sh
+++ install-build-agent.sh
@@ -14,12 +14,28 @@ apt-get update && apt-get install -y --no-install-recommends \
         iputils-ping \
         libunwind8 \
         netcat \
+        gnupg \
         build-essential
 
 # Install Docker CE
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install Bazel
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update && sudo apt install bazel
+
+# Install az CLI
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
+
+# az CLI DevOps extension
+az extension add --name azure-devops
 
 # Move script(s) to final destination
 mkdir -p /scripts
